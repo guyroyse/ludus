252870d38182529d50722a82817eaa8bdfc96ce8
Guy Royse <guy@guyroyse.com>
2011-02-24 01:14:39 -0500

    Frustration in trying to get JRuby to invoke the correct methods via reflection.

--- src/app.rb
+++ src/app.rb
@@ -3,6 +3,7 @@ require 'sinatra'
 require 'java'
 
 java_import com.google.appengine.api.users.UserServiceFactory
+java_import java.util.Set
 
 get '/app/hello' do
   "Hello World"
@@ -13,10 +14,15 @@ get '/_ah/login_required' do
   user = user_service.current_user
   
   if user.nil?
-    login_url = user_service.create_login_url(request.url, nil, 'google.com/accounts/o8/id', {});
+    user_service.java_class.declared_methods.each do |method|
+      if method.name == 'createLoginURL' && method.parameter_types.length == 4 then
+        login_url = method.invoke user_service.java_object, request.url, nil, 'google.com/accounts/o8/id', nil
+      end 
+    end
     content = "<a href='#{login_url}'>Login Using Google</a>"
   else
-    logout_url = user_service.create_logout_url(request.url)
+    create_logout_url = user_service.java_class.method 'create_logout_url', java.lang.String
+    logout_url = create_logout_url.invoke  request.url
     content = "Hello #{user.nickname}!"
     content += "<a href='#{logout_url}'>Logout</a>"
   end
