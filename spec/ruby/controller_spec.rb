476af8f7216edf6e7cd100f284d04f81b42c3edc
Guy Royse <guy@guyroyse.com>
2011-02-28 22:47:11 -0500

    Refactoring test for clarity -- not achieved yet

--- spec/ruby/controller_spec.rb
+++ spec/ruby/controller_spec.rb
@@ -4,30 +4,43 @@ require 'controllers'
 
 describe LoginUrlController do
   it 'returns JSON containing expected provider URLs' do
+    
+    REQUESTED_URL = 'http://example.com/foo'
+    REQUESTED_PATH = '/foo'
+    RETURN_URL = 'http://example.com/bar'
+    PROVIDER_NAME = 'foo'
+    PROVIDER_IMAGE = 'bar.png'
+    PROVIDER_URL = 'http://example.com/baz'
+    
     def LoginUrlController::mock_url=(mock)
       @mock_url = mock
     end
     def LoginUrlController::url(requested_url, requested_path)
-      requested_url.should == 'http://example.com/foo'
-      requested_path.should == '/foo'
+      requested_url.should == REQUESTED_URL
+      requested_path.should == REQUESTED_PATH
       @mock_url
     end
-    mock_url = mock('url')
-    mock_url.should_receive(:url).once.and_return('http://example.com/bar')
-    LoginUrlController::mock_url = mock_url
     
     def LoginUrlController::mock_providers=(mock)
       @mock_providers = mock
     end
     def LoginUrlController::providers(url)
-      url.should == 'http://example.com/bar'
+      url.should == RETURN_URL
       @mock_providers
     end
+    
+    mock_url = mock('url')
+    mock_url.should_receive(:url).once.and_return(RETURN_URL)
+    LoginUrlController::mock_url = mock_url
+    
     mock_provider = mock('provider')
-    mock_provider.should_receive(:to_hash).once.and_return({ :name => 'foo', :image => 'bar.png', :url => 'http://example.com/baz' });
+    mock_provider.should_receive(:to_hash).once.and_return({ :name => PROVIDER_NAME, :image => PROVIDER_IMAGE, :url => PROVIDER_URL });
     mock_providers = mock('providers')
     mock_providers.should_receive(:each).once.and_yield(mock_provider)    
-    LoginUrlController::mock_providers = mock_providers  
-    LoginUrlController.execute('http://example.com/foo', '/foo').should == '[{"name":"foo","image":"bar.png","url":"http://example.com/baz"}]'
+    LoginUrlController::mock_providers = mock_providers
+      
+    json = LoginUrlController.execute(REQUESTED_URL, REQUESTED_PATH)
+    
+    json.should == %Q/[{"name":"#{PROVIDER_NAME}","image":"#{PROVIDER_IMAGE}","url":"#{PROVIDER_URL}"}]/
   end
 end
