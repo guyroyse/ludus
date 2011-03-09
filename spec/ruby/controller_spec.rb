69492534e6e320c3b830b6c6e531aeab4ca37cf1
Guy Royse <guy@guyroyse.com>
2011-03-09 04:50:39 -0500

    Refactored controller tests

--- spec/ruby/controller_spec.rb
+++ spec/ruby/controller_spec.rb
@@ -4,71 +4,23 @@ require 'controllers'
 
 describe LoginUrlController do
   
-  REQUESTED_URL = 'http://example.com/foo'
-  REQUESTED_PATH = '/foo'
-  RETURN_URL = 'http://example.com/bar'
-  PROVIDER_NAME = 'foo'
-  PROVIDER_IMAGE = 'bar.png'
-  PROVIDER_URL = 'http://example.com/baz'
-  
-  before do
-    
-    @controller = LoginUrlController.new()
-    
-    def @controller.url_double=(double)
-      @url_double = double
-    end
-    
-    def @controller.url(requested_url, requested_path)
-      @url_double
-    end
-    
-    def @controller.providers_double=(double)
-      @providers_double = double
-    end
-    
-    def @controller.providers(url)
-      @providers_double
-    end
-    
-    @controller.url_double = stub('url', :url => RETURN_URL)
-    
-    provider_stub = stub('provider', :to_hash => { :name => PROVIDER_NAME, :image => PROVIDER_IMAGE, :url => PROVIDER_URL });
-    providers_stub = stub('providers')
-    providers_stub.should_receive(:each).and_yield(provider_stub)
-    @controller.providers_double = providers_stub
-      
-  end
-  
   it 'returns JSON containing expected provider URLs' do
     
-    json = @controller.execute(REQUESTED_URL, REQUESTED_PATH)
+    REQUESTED_URL = 'http://example.com/foo'
+    REQUESTED_PATH = '/foo'
+    PROVIDER_NAME = 'foo'
+    PROVIDER_IMAGE = 'bar.png'
+    PROVIDER_URL = 'http://example.com/baz'
+    
+    provider = stub('provider', :to_hash => { :name => PROVIDER_NAME, :image => PROVIDER_IMAGE, :url => PROVIDER_URL });
+    providers = stub('providers')
+    providers.should_receive(:each).and_yield(provider)
+    OpenIdProviders.should_receive(:new).and_return(providers)
+
+    json = LoginUrlController.new.execute(REQUESTED_URL, REQUESTED_PATH)
     
     json.should == %Q/[{"name":"#{PROVIDER_NAME}","image":"#{PROVIDER_IMAGE}","url":"#{PROVIDER_URL}"}]/
     
   end
-  
-  it 'passes request correct URL data to LudusUrl' do
 
-    def @controller.url(requested_url, requested_path)
-      requested_url.should == REQUESTED_URL
-      requested_path.should == REQUESTED_PATH
-      @url_double
-    end
-    
-    @controller.execute REQUESTED_URL, REQUESTED_PATH
-        
-  end
-  
-  it 'passed correct URL to OpenIdProviders' do
-    
-    def @controller.providers(url)
-      url.should == RETURN_URL
-      @providers_double
-    end
-    
-    @controller.execute REQUESTED_URL, REQUESTED_PATH
-    
-  end
-  
 end
--- src/ruby/controllers.rb
+++ src/ruby/controllers.rb
@@ -4,17 +4,11 @@ require 'model'
 class LoginUrlController
   def execute(requested_url, requested_path)
     retval = []
-    return_url = url(requested_url, requested_path)
-    providers = providers(return_url.url) 
+    return_url = LudusUrl.new(requested_url, requested_path)
+    providers = OpenIdProviders.new(return_url.url) 
     providers.each do |provider|
       retval << provider.to_hash
     end
     retval.to_json    
-  end
-  def url(requested_url, requested_path)
-    LudusUrl.new requested_url, requested_path
-  end
-  def providers(url)
-    OpenIdProviders.new url
   end
 end
