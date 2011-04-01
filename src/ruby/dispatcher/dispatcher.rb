ea8f2119e2a43ec5a9755e2d027baad996f0e473
Guy Royse <guy@guyroyse.com>
2011-04-01 00:12:37 -0400

    Some changes for dispatcher

--- src/ruby/dispatcher/dispatcher.rb
+++ src/ruby/dispatcher/dispatcher.rb
@@ -1,20 +1,20 @@
 require 'rubygems'
 require 'sinatra'
-require 'java'
 require 'controllers'
 
-java_import com.hardboiledgeek.ludus.service.LudusUserService
+HOME_URL = '/home.html'
+LOBBY_URL = '/ludus.html'
 
 get '/_ah/login_required' do
-  redirect LoginController.new.login_required_redirect('/home.html', '/ludus.html')
+  redirect LoginController.new.login_required_redirect(HOME_URL, LOBBY_URL)
 end
 
 get '/public/api/login-urls' do
-  json_api { LoginController.new.login_urls request.url, request.path, '/ludus.html' }
+  json_api { LoginController.new.login_urls request.url, request.path, LOBBY_URL }
 end
 
 get '/api/logout-url' do
-  json_api { LoginController.new.logout_url '/home.html' }
+  json_api { LoginController.new.logout_url HOME_URL }
 end
 
 get '/public/api/current-user' do
